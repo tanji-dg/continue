b4ea9d686f2d0718ba89adaaecdf2a70ac6f1d51
Hideki Tanji <tanji@daitogiken.com>
2025-03-17 11:37:17 +0900

    WIP

---
+++ env.sh
@@ -0,0 +1,5 @@
+export CLAUDE_BRIDGE_PROVIDER="openai"
+export CLAUDE_BRIDGE_MODEL="us.deepseek.r1-v1:0"
+export CLAUDE_BRIDGE_API_KEY="apiKey"
+export CLAUDE_BRIDGE_BASE_URL="http://192.168.35.182:8000/api/v1"
+export PATH=/mnt/d/git/sysu-gitlab/lemmy/apps/claude-bridge:$PATH
