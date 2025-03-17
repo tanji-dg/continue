b5b9320985da0f31fa28b9465ac84d09d395c344
Hideki Tanji <tanji@daitogiken.com>
2025-03-17 11:37:17 +0900

    WIP

--- core/context/retrieval/pipelines/BaseRetrievalPipeline.ts
+++ core/context/retrieval/pipelines/BaseRetrievalPipeline.ts
@@ -1,4 +1,5 @@
 // @ts-ignore
+import kuromoji from "kuromoji";
 import nlp from "wink-nlp-utils";
 
 import { BranchAndDir, Chunk, ContinueConfig, IDE, ILLM } from "../../../";
@@ -31,9 +32,86 @@ export interface IRetrievalPipeline {
   run(args: RetrievalPipelineRunArguments): Promise<Chunk[]>;
 }
 
+class NLPProcessor {
+  private tokenizer: kuromoji.Tokenizer<string> | null = null;
+
+  constructor() {
+        // 正しい辞書パスを取得
+        const dicPath = vscode.Uri.joinPath(context.extensionUri, "media", "dict").fsPath;
+
+        // Kuromoji の辞書を読み込む
+        kuromoji.builder({ dicPath }).build((err, tokenizer) => {
+            if (err) {
+                console.error("Kuromoji tokenizer error:", err);
+                return;
+            }
+            console.log("Kuromoji tokenizer initialized.");
+        });
+    kuromoji.builder({ dicPath: "node_modules/kuromoji/dict" }).build((err, tokenizer) => {
+      if (err) throw err;
+      this.tokenizer = tokenizer;
+    });
+  }
+
+  private isJapanese(text: string): boolean {
+    return /[一-龠ぁ-んァ-ン]/.test(text);
+  }
+
+  getCleanedTrigrams(query: string): string[] {
+    if (this.isJapanese(query)) {
+      return this.getJapaneseTrigrams(query);
+    } else {
+      return this.getEnglishTrigrams(query);
+    }
+  }
+
+  private getJapaneseTrigrams(query: string): string[] {
+    if (!this.tokenizer) throw new Error("Tokenizer not initialized");
+
+    // 1. 形態素解析
+    const tokens = this.tokenizer.tokenize(query);
+
+    // 2. 名詞・動詞を抽出
+    const words = tokens
+      .filter(token => token.pos === "名詞" || token.pos === "動詞")
+      .map(token => token.basic_form || token.surface_form);
+
+    // 3. ストップワードを除去
+    const stopwords = new Set(["の", "は", "が", "を", "に", "で", "と", "も", "する", "なる"]);
+    const filteredWords = words.filter(word => !stopwords.has(word));
+
+    // 4. 重複削除
+    const uniqueWords = [...new Set(filteredWords)];
+
+    // 5. 3-gram の生成
+    const trigrams = uniqueWords.length < 3
+      ? []
+      : uniqueWords.map((_, i, arr) => arr.slice(i, i + 3).join(" ")).filter(trigram => trigram.split(" ").length === 3);
+
+    return trigrams;
+  }
+
+  private getEnglishTrigrams(query: string): string[] {
+    let text = nlp.string.removeExtraSpaces(query);
+    text = nlp.string.stem(text);
+
+    let tokens = nlp.string
+      .tokenize(text, true)
+      .filter((token: any) => token.tag === "word")
+      .map((token: any) => token.value);
+
+    tokens = nlp.tokens.removeWords(tokens);
+    tokens = nlp.tokens.setOfWords(tokens);
+
+    const cleanedTokens = [...tokens].join(" ");
+    return nlp.string.ngram(cleanedTokens, 3);
+  }
+}
+
 export default class BaseRetrievalPipeline implements IRetrievalPipeline {
   private ftsIndex = new FullTextSearchCodebaseIndex();
   private lanceDbIndex: LanceDbIndex | null = null;
+  private processor = new NLPProcessor();
 
   constructor(protected readonly options: RetrievalPipelineOptions) {
     void this.initLanceDb();
@@ -54,21 +132,7 @@ export default class BaseRetrievalPipeline implements IRetrievalPipeline {
   private getCleanedTrigrams(
     query: RetrievalPipelineRunArguments["query"],
   ): string[] {
-    let text = nlp.string.removeExtraSpaces(query);
-    text = nlp.string.stem(text);
-
-    let tokens = nlp.string
-      .tokenize(text, true)
-      .filter((token: any) => token.tag === "word")
-      .map((token: any) => token.value);
-
-    tokens = nlp.tokens.removeWords(tokens);
-    tokens = nlp.tokens.setOfWords(tokens);
-
-    const cleanedTokens = [...tokens].join(" ");
-    const trigrams = nlp.string.ngram(cleanedTokens, 3);
-
-    return trigrams;
+    return this.processor.getCleanedTrigrams(query);
   }
 
   private escapeFtsQueryString(query: string): string {
