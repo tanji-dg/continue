f870d91dae2725513920ed9d00dcde1bbd33510e
丹治秀樹 <tanji@daitogiken.local>
2025-04-10 17:57:05 +0900

    Applyが正常に動作するように修正。

--- core/llm/index.ts
+++ core/llm/index.ts
@@ -29,7 +29,7 @@ import {
   TemplateType,
 } from "../index.js";
 import mergeJson from "../util/merge.js";
-import { renderChatMessage } from "../util/messageContent.js";
+import { renderChatMessage, stripImages } from "../util/messageContent.js";
 import { isOllamaInstalled } from "../util/ollamaHelper.js";
 import { Telemetry } from "../util/posthog.js";
 import { withExponentialBackoff } from "../util/withExponentialBackoff.js";
@@ -635,6 +635,14 @@ export abstract class BaseLLM implements ILLM {
     };
   }
 
+  private async *_streamRawCompleteChunk(
+    prompt: string,
+    signal: AbortSignal,
+    options: CompletionOptions,
+  ): AsyncGenerator<ChatMessage> {
+    throw new Error("Not implemented");
+  }
+
   async *streamComplete(
     _prompt: string,
     signal: AbortSignal,
@@ -670,6 +678,7 @@ export abstract class BaseLLM implements ILLM {
     }
 
     let completion = "";
+    let thinking = undefined;
     try {
       if (this.shouldUseOpenAIAdapter("streamComplete") && this.openaiAdapter) {
         if (completionOptions.stream === false) {
@@ -698,6 +707,28 @@ export abstract class BaseLLM implements ILLM {
             yield content;
           }
         }
+      } else if (BaseLLM.isBedrockDeepSeekR1Model(this.title)) {
+        for await (const chunk of this._streamRawComplete(
+          prompt,
+          signal,
+          completionOptions,
+        )) {
+          if (chunk.role === 'assistant') {
+            const content = stripImages(chunk.content);
+            completion += content;
+            yield content;
+          } else if (chunk.role === 'thinking') {
+            const content = stripImages(chunk.content);
+            if (!thinking) {
+              thinking = "";
+            }
+            thinking += content;
+          }
+          interaction?.logItem({
+            kind: "message",
+            message: chunk,
+          });
+        }
       } else {
         for await (const chunk of this._streamComplete(
           prompt,
@@ -716,7 +747,7 @@ export abstract class BaseLLM implements ILLM {
         completionOptions.model,
         prompt,
         completion,
-        undefined,
+        thinking,
         interaction,
       );
     } catch (e) {
@@ -748,6 +779,10 @@ export abstract class BaseLLM implements ILLM {
       completion,
       completionOptions,
     };
+  }
+
+  public static isBedrockDeepSeekR1Model(title: string | undefined): boolean {
+    return title === 'Bedrock: DeepSeek-R1';
   }
 
   async complete(
@@ -966,16 +1001,22 @@ export abstract class BaseLLM implements ILLM {
             completionOptions,
           )) {
             if (chunk.role === "assistant") {
+<<<<<<< HEAD
               completion += this._formatChatMessage(chunk);
+              yield chunk;
+=======
+              const content = stripImages(chunk.content);
+              completion += content;
+              yield chunk;
+>>>>>>> 0882807cc (Applyが正常に動作するように修正。)
             } else if (chunk.role === "thinking") {
-              thinking += chunk.content;
+              const content = stripImages(chunk.content);
+              thinking += content;
             }
-
             interaction?.logItem({
               kind: "message",
               message: chunk,
             });
-            yield chunk;
           }
         }
       }
@@ -1097,6 +1138,14 @@ export abstract class BaseLLM implements ILLM {
     signal: AbortSignal,
     options: CompletionOptions,
   ): AsyncGenerator<string> {
+    throw new Error("Not implemented");
+  }
+
+  protected async *_streamRawComplete(
+    prompt: string,
+    signal: AbortSignal,
+    options: CompletionOptions,
+  ): AsyncGenerator<ChatMessage> {
     throw new Error("Not implemented");
   }
 
