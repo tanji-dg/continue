bd170a05a246d72febac3b6560aa76af5babc850
丹治秀樹 <tanji@daitogiken.local>
2025-04-11 14:43:54 +0900

    調整。

--- core/llm/index.ts
+++ core/llm/index.ts
@@ -804,7 +804,7 @@ export abstract class BaseLLM implements ILLM {
   }
 
   public static isSupportPromptCacheModel(model: string): boolean {
-    return model.includes("claude");
+    return model.includes("claude-3-7");
   }
 
   async complete(
