4dc76b5e0c8f43b74ab06d2e3bbc14cfb4ba2ecd
Hideki Tanji <tanji@daitogiken.com>
2025-04-11 14:11:36 +0900

    バグ修正。

--- core/llm/llms/Bedrock.ts
+++ core/llm/llms/Bedrock.ts
@@ -75,7 +75,7 @@ class Bedrock extends BaseLLM {
       region: options.region,
       headers: {},
     };
-    if (!BaseLLM.isBedrockDeepSeekR1Model(this.title)) {
+    if (BaseLLM.isSupportPromptCacheModel(this.model)) {
       this.cacheBehavior = { cacheSystemMessage: true, cacheConversation: true };
     }
   }
