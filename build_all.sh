6bdc402f0a6fe9639d450611867ef9fab2d972da
丹治秀樹 <tanji@daitogiken.local>
2025-04-23 12:35:14 +0900

    ビルドスクリプト追加。

---
+++ build_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+# nvmの読み込み（通常のインストールパス）
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+cd extensions/vscode/
+nvm use v20.11.0 
+npm run esbuild
+npm run package-all
+cp build/*.vsix ~/d-out-tanji
