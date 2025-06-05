01bbdb227889a80c4d6e7d66d0993523f4cd01c1
丹治秀樹 <tanji@daitogiken.local>
2025-06-05 11:03:48 +0900

    WIP

--- core/context/retrieval/pipelines/BaseRetrievalPipeline.ts
+++ core/context/retrieval/pipelines/BaseRetrievalPipeline.ts
@@ -222,9 +222,7 @@ export default class BaseRetrievalPipeline implements IRetrievalPipeline {
       return [];
     }
 
-    await this.processor.waitForTokenizerBuild();
-
-	const tokens = this.getCleanedTrigrams(args.query).join(" OR ");
+    const tokens = this.getCleanedTrigrams(args.query).join(" OR ");
 
     return await this.ftsIndex.retrieve({
       n,
